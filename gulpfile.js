788ab79513daa5d420d4d12424b0743e04130ed9
J. Cohen <hostelmarketing@gmail.com>
2015-06-18 15:08:45 -0700

    Add gulpfile.js. Move macros to ./macros

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+// Borrowed from https://github.com/simianhacker/code-by-voice/blob/master/Gulpfile.js
+// Copies macros from the git repo into \Natlink\Natlink\MacroSystem
+var gulp = require('gulp');
+var path = require('path');
+var source = path.join('.', 'macros', '**', '*');
+var destination = '\\NatLink\\NatLink\\MacroSystem';
+
+gulp.task('copy-macros', function () {
+    console.log("Copying macros");
+    gulp.src(source).pipe(gulp.dest(destination));
+});
+
+gulp.task('default', function () {
+    gulp.run('copy-macros');
+    gulp.watch(source, function (event) {
+        gulp.run('copy-macros');
+    });
+});
